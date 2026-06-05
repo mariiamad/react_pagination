421887f6c084b9e5481c6dcbbbddb2dbc42dce52
Mariia Madarash <mariiamadarash@gmail.com>
2026-06-05 15:50:27 +0200

    Solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -50,7 +50,7 @@ export const Pagination = ({
             data-cy="pageLink"
             className="page-link"
             href={`#${page}`}
-            onClick={() => onPageChange(page)}
+            onClick={() => page !== currentPage && onPageChange(page)}
           >
             {page}
           </a>
